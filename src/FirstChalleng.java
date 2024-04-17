3a455ee9f38db63d94aef25526b959856fbf67d1
eurianealvesdev <anysevla.ea@gmail.com>
2024-04-17 11:54:13 -0300

    Initial commit

---
+++ src/FirstChalleng.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Firs Challeng {
+    public static void Fire Challeng (String[] args) {
+
+        System.out.println("Bem vindo ou seu Extrato Bancário");
+        System.out.println("**********************************");
+        System.out.println( "Dados iniciais do cliente:");
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+    }
+}
